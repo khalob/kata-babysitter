b88265cf9aa006da7b4c4c864b7a97eeff329dac
khalob <kcognata@emich.edu>
2019-05-05 13:49:56 -0400

    Cannot use times before 5pm and after 4am

--- timeUtil.js
+++ timeUtil.js
@@ -13,6 +13,21 @@ var timeUtil = {
 			if (minutes > 0){
 				return 'Cannot use fractional hours';
 			}
+			
+			if (meridian.toLowerCase() === 'am') {
+				if (hours > 4) {
+					return 'Cannot use a time after 4:00AM';
+				} else {
+					result = hours + 8;
+				}
+			} else {
+				if (hours < 5) {
+					return 'Cannot use a time before 5:00PM';
+				} else {
+					result = hours - 4;
+				}
+			}
+			
 			return result;
 		}
 		return 'Invalid time given, must be a String object in HH:MMAA format (e.g. 11:00PM)'
