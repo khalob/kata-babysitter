c432d864abeecffe33da958728a6369e6ede2fb0
khalob <kcognata@emich.edu>
2019-05-08 20:46:05 -0400

    Add function to neatly print out the total pay of a night spent babysitting

--- class/babysitter.js
+++ class/babysitter.js
@@ -27,4 +27,16 @@ Babysitter.prototype.calculatePay = function (startTime, endTime, family) {
 	return totalPay;
 };
 
+/**
+ * Prints the total pay of a night spent babysitting, based on a start time, end time, and family.
+ *
+ * @param startTime {string} - The start of the time range (e.g. '12:00AM')
+ * @param endTime {string} - The end of the time range (e.g. '4:00AM')
+ * @param family {Family} - Family sat for. Holds payrate information and family name.
+ */
+Babysitter.prototype.logPay = function (startTime, endTime, family) {
+	var totalPay = this.calculatePay(startTime, endTime, family);
+	console.log('Babysitting from ' + startTime + ' to ' + endTime + ' at the ' + family.name +'\'s house, ' +  this.name + ' made: $' + totalPay);
+};
+
 module.exports = Babysitter;
